1e77fde83f6d0e5033c6f35c6e02b187aeda0a94
Isaac Castro G <87621852+IsaaChaquito@users.noreply.github.com>
2023-12-24 18:42:41 -0600

    commiteando ando

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -1,5 +1,10 @@
 <template>
-    <h3>Pokemon Options</h3>
+    <div class="options-container">
+        <option value="">Opcion 1</option>
+        <option value="">Opcion 2</option>
+        <option value="">Opcion 3</option>
+        <option value="">Opcion 4</option>
+    </div>
 </template>
 
 <script>
@@ -8,6 +13,21 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+    .options-container{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    option{
+        width: 300px;
+        height: 10px;
+        padding: 10px;
+        background: red;
+        margin: 10px;
+    }
 
 </style>
